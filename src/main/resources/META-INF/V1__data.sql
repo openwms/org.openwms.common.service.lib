a9eb561c5479fe72de71f044f916422f889be80c
Heiko Scherrer <scherrer@openwms.org>
2016-11-04 19:01:13 +0100

    enabled flyway

---
+++ src/main/resources/META-INF/V1__data.sql
@@ -0,0 +1,3 @@
+insert into com_location_type (c_pk,c_pid,c_type,c_description,c_height,c_length,c_width,c_ol,c_created,c_updated) values (0,326981811784,'FG','Flat Good Location',20,30,50,0,now(),now());
+insert into com_location_type (c_pk,c_pid,c_type,c_description,c_height,c_length,c_width,c_ol,c_created,c_updated) values (1,404376159041,'PG','Pallet Good Location',250,120,120,1,now(),now());
+insert into com_location_type (c_pk,c_pid,c_type,c_description,c_height,c_length,c_width,c_ol,c_created,c_updated) values (2,268461148413,'HG','Hanging Good Location',300,10,50,2,now(),now());
