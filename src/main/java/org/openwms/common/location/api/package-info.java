917f85bdd99959e5c435e7520e01765c104cfc6d
Heiko Scherrer <scherrer@openwms.org>
2017-06-28 06:52:36 +0200

    point to fixed version of CORE deps added some API docs
    Signed-off-by: Heiko Scherrer <scherrer@openwms.org>

---
+++ src/main/java/org/openwms/common/location/api/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the public API to deal with Locations and LocationGroups.
+ */
+package org.openwms.common.location.api;
---
+++ src/main/java/org/openwms/common/transport/api/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * This package contains the public API to deal with TransportUnits.
+ */
+package org.openwms.common.transport.api;
