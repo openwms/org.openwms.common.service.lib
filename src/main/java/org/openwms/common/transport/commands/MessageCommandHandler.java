ec3bd1eb4e2c00b2406fe86fb9ca4b8940974e57
Heiko Scherrer <hscherrer@interface21.io>
2019-07-31 22:51:18 +0200

    Site checkin for project OpenWMS.org COMMON: Services

--- src/main/java/org/openwms/common/transport/commands/MessageCommandHandler.java
+++
@@ -1,8 +0,0 @@
-package org.openwms.common.transport.commands;
-
-import org.openwms.common.transport.api.commands.MessageCommand;
-
-public interface MessageCommandHandler {
-
-    void handle(MessageCommand command);
-}
