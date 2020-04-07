fd9c109079e3c49b30388e5a10a69c220ad6ce20
Heiko Scherrer <scherrer@openwms.org>
2020-04-07 23:42:22 +0200

    documentation

--- doc/Config.groovy
+++ doc/Config.groovy
@@ -7,7 +7,7 @@ outputPath = 'build'
 inputPath = '.'
 
 inputFiles = [
-        [file: 'arc42-template.adoc', formats: ['html','pdf','docbook']],
+        [file: 'index.adoc', formats: ['html','pdf','docbook']],
         [file: 'ppt/Demo.pptx.ad', formats: ['revealjs']]
              ]
 
@@ -15,7 +15,7 @@ taskInputsDirs = ["${inputPath}/src",
                   "${inputPath}/images",
                  ]
 
-taskInputsFiles = ["${inputPath}/arc42-template.adoc"]
+taskInputsFiles = ["${inputPath}/index.adoc"]
 
 confluence = [:]
 confluence.with {
@@ -28,5 +28,13 @@ confluence.with {
     preambleTitle = 'Technical Architecture'
     pageSuffix = ''
     credentials = "${System.getenv('ATLASSIAN_USER')}:${System.getenv('ATLASSIAN_PASSWORD')}".bytes.encodeBase64().toString()
-    extraPageContent = '<ac:structured-macro ac:name="warning"><ac:parameter ac:name="title" /><ac:rich-text-body>This is a generated page, do not edit!</ac:rich-text-body></ac:structured-macro>'
+//    extraPageContent = '<ac:structured-macro ac:name="warning"><ac:parameter ac:name="title" /><ac:rich-text-body>This is a generated page, do not edit!</ac:rich-text-body></ac:structured-macro>'
+}
+
+
+github = [:]
+github.with {
+    user = "${System.getenv('GITHUB_USER')}"
+    password = "${System.getenv('GITHUB_PASSWORD')}"
+    root = "https://api.github.com/"
 }
