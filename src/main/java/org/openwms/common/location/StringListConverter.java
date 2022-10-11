da879760182d6884212dec07fba33e0f28290135
Heiko Scherrer <hscherrer@interface21.io>
2022-10-11 14:33:42 +0200

    Aligned StringListConverter with other implementations; docs

--- src/main/java/org/openwms/common/location/StringListConverter.java
+++ src/main/java/org/openwms/common/location/StringListConverter.java
@@ -18,32 +18,44 @@ package org.openwms.common.location;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import javax.persistence.PersistenceException;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static java.lang.String.format;
 
 /**
- * A StringListConverter is a JPA {@link AttributeConverter} that is able to convert a String into a List of Strings and vice-versa.
+ * A StringListConverter is a JPA {@link AttributeConverter} that is able to convert a {@code String} into a {@code List} of strings and
+ * vice-versa.
  *
  * @author Heiko Scherrer
  */
 @Converter
 public class StringListConverter implements AttributeConverter<List<String>, String> {
 
-    /** Default length of a {@code StringList}. */
+    /** Default length of the string list. */
     public static final int STRING_LIST_LENGTH = 1024;
-    /** Default separator sign of a {@code StringList}. */
+    /** Default separator sign of a string list. */
     public static final String SEPARATOR = ",";
 
     /**
-     * Return the maximum possible list length.
+     * Return the maximum list length.
      *
+     * @see #STRING_LIST_LENGTH
      * @return The maximum list length
      */
     protected int getListLength() {
         return STRING_LIST_LENGTH;
+    }
+
+    /**
+     * Return the separator that is used to separate the String into a List of Strings.
+     *
+     * @see #SEPARATOR
+     * @return The separator as String
+     */
+    protected String getSeparator() {
+        return SEPARATOR;
     }
 
     /**
@@ -54,9 +66,10 @@ public class StringListConverter implements AttributeConverter<List<String>, Str
         if (attribute == null) {
             return null;
         }
-        String join = String.join(SEPARATOR, attribute);
+        var join = String.join(getSeparator(), attribute);
         if (join.length() > getListLength()) {
-            throw new PersistenceException(format("Length of column is exceeded, actual length is [%s]", join.length()));
+            throw new PersistenceException(format("Length of column exceeded, actual length is [%d], allowed is [%d]", join.length(),
+                    getListLength()));
         }
         return join;
     }
@@ -66,9 +79,9 @@ public class StringListConverter implements AttributeConverter<List<String>, Str
      */
     @Override
     public List<String> convertToEntityAttribute(String dbData) {
-        if (dbData == null || "".equals(dbData)) {
-            return Collections.emptyList();
+        if (dbData == null || dbData.isEmpty()) {
+            return new ArrayList<>();
         }
-        return Arrays.asList(dbData.split(SEPARATOR));
+        return new ArrayList<>(Arrays.asList(dbData.split(getSeparator())));
     }
 }
