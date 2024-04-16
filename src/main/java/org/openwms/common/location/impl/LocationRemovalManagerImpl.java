a9083e09b09cf7881910a108d75139f90180990b
Heiko Scherrer <scherrer@openwms.org>
2024-04-16 14:30:10 +0200

    #99 Don't use ServiceRegistry because service may be down

--- src/main/java/org/openwms/common/location/impl/LocationRemovalManagerImpl.java
+++ src/main/java/org/openwms/common/location/impl/LocationRemovalManagerImpl.java
@@ -28,17 +28,18 @@ import org.openwms.common.location.impl.registration.ReplicaRegistry;
 import org.openwms.core.listener.RemovalNotAllowedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 
 import static java.lang.String.format;
 import static org.openwms.common.CommonMessageCodes.LOCATION_NOT_FOUND_BY_PKEY;
@@ -79,7 +80,14 @@ class LocationRemovalManagerImpl implements LocationRemovalManager {
             LOGGER.warn("Location with pKey [{}] shall be deleted but it does not exist", pKey);
             throw new NotFoundException(translator, LOCATION_NOT_FOUND_BY_PKEY, new String[]{pKey}, pKey);
         }
-        deleteInternal(locationOpt.get());
+        try {
+            deleteInternal(locationOpt.get());
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            // if any failure occurs, send a persistent async message to release the deletion for everyone
+            eventPublisher.publishEvent(new DeletionFailedEvent(locationOpt.get().getPersistentKey()));
+            throw new RemovalNotAllowedException(e.getMessage());
+        }
     }
 
     /**
@@ -95,31 +103,27 @@ class LocationRemovalManagerImpl implements LocationRemovalManager {
         verifyNoTransportUnitsOn(location);
         var registeredServices = registrationService.getAllRegistered();
 
+        if (registeredServices.isEmpty()) {
+            LOGGER.info("No services registered that are requested for removal");
+        }
         // first ask all services and call the requestRemovalEndpoint
         // might throw RemovalNotAllowedException and exit
         for (var srv : registeredServices) {
-            var si = dc.getInstances(srv.getApplicationName());
-            askForRemoval(si, srv, List.of(location.getPersistentKey()));
+            askForRemoval(srv, List.of(location.getPersistentKey()));
         }
 
         // if all are fine then call the removeEndpoint to mark the Location as deleted and not be visible in the foreign service
-        try {
-            for (var srv : registeredServices) {
-                var si = dc.getInstances(srv.getApplicationName());
-                removeLocation(si, srv, List.of(location.getPersistentKey()));
-            }
-            deleteLocation(location);
-        } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
-            // if any failure occurs, send a persistent async message to release the deletion for everyone
-            eventPublisher.publishEvent(new DeletionFailedEvent(location.getPersistentKey()));
+        for (var srv : registeredServices) {
+            removeLocation(srv, List.of(location.getPersistentKey()));
         }
+        deleteLocation(location);
     }
 
     private void verifyNoTransportUnitsOn(Location location) {
         if (repository.doesTUonLocationExists(location.getPk())) {
             throw new RemovalNotAllowedException("TransportUnit exist on Location [%s]".formatted(location.getLocationId()));
         }
+        LOGGER.info("No TransportUnits exist on Location [{}]", location.getLocationId());
     }
 
     private void verifyNoTransportUnitsOn(Collection<String> pKeys) {
@@ -134,7 +138,6 @@ class LocationRemovalManagerImpl implements LocationRemovalManager {
         eventPublisher.publishEvent(LocationEvent.of(location, LocationEvent.LocationEventType.DELETED));
         // and finally delete the Location
         repository.delete(location);
-
     }
 
     /**
@@ -152,8 +155,7 @@ class LocationRemovalManagerImpl implements LocationRemovalManager {
             // first ask all services and call the requestRemovalEndpoint
             // might throw RemovalNotAllowedException and exit
             for (var srv : registeredServices) {
-                var si = dc.getInstances(srv.getApplicationName());
-                askForRemoval(si, srv, pKeys);
+                askForRemoval(srv, pKeys);
             }
             LOGGER.debug("It's allowed to delete the given Locations");
             return true;
@@ -175,7 +177,7 @@ class LocationRemovalManagerImpl implements LocationRemovalManager {
         var registeredServices = registrationService.getAllRegistered();
         try {
             for (var srv : registeredServices) {
-                removeLocation(dc.getInstances(srv.getApplicationName()), srv, pKeys);
+                removeLocation(srv, pKeys);
             }
             LOGGER.debug("All Locations marked for deletion");
         } catch (Exception e) {
@@ -188,54 +190,51 @@ class LocationRemovalManagerImpl implements LocationRemovalManager {
         }
     }
 
-    private void askForRemoval(List<ServiceInstance> sis, ReplicaRegistry srv, Collection<String> pKeys) {
-        for (var si : sis) {
-            boolean result;
-            var endpoint = si.getMetadata().get("protocol") + "://" + si.getServiceId() + srv.getRequestRemovalEndpoint();
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("Request for removal to the service with API [{}]", endpoint);
+    private void askForRemoval(ReplicaRegistry srv, Collection<String> pKeys) {
+        var headers = new HttpHeaders();
+        boolean result;
+        var endpoint = "http://" + srv.getApplicationName() + srv.getRequestRemovalEndpoint();
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Request for removal to the service with API [{}]", endpoint);
+        }
+        try {
+            var response = aLoadBalanced.exchange(
+                    endpoint,
+                    HttpMethod.POST,
+                    new HttpEntity<List<String>>(new ArrayList<>(pKeys), headers),
+                    Boolean.class
+            );
+            result = response.getBody() != null && response.getBody();
+            if (result) {
+                LOGGER.info("Service [{}] allows to remove all Locations", srv.getApplicationName());
+            } else {
+                LOGGER.info("Service [{}] does not allow to remove Locations", srv.getApplicationName());
             }
-            try {
-                var response = aLoadBalanced.exchange(
-                        endpoint,
-                        HttpMethod.GET,
-                        null,
-                        Boolean.class,
-                        Map.of("pKey", pKeys)
-                );
-                result = response.getBody() != null && response.getBody();
-                if (result) {
-                    LOGGER.info("Service [{}] allows to remove all Locations", si.getServiceId());
-                } else {
-                    LOGGER.info("Service [{}] does not allow to remove Locations", si.getServiceId());
-                }
-            } catch (Exception e) {
-                throw new RemovalNotAllowedException("Exception. Removal of Locations is declined by service [%s]".formatted(si.getServiceId()));
-            }
-            if (!result) {
-                throw new RemovalNotAllowedException("Removal of Locations has been declined by service [%s]".formatted(si.getServiceId()));
-            }
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage(), e);
+            throw new RemovalNotAllowedException("Exception. Removal of Locations is declined by service [%s]".formatted(srv.getApplicationName()));
+        }
+        if (!result) {
+            throw new RemovalNotAllowedException("Removal of Locations has been declined by service [%s]".formatted(srv.getApplicationName()));
         }
     }
 
-    private void removeLocation(List<ServiceInstance> sis, ReplicaRegistry srv, Collection<String> pKeys) {
-        for (var si : sis) {
-            var endpoint = si.getMetadata().get("protocol") + "://" + si.getServiceId() + srv.getRemovalEndpoint();
-            if (LOGGER.isDebugEnabled()) {
-                LOGGER.debug("Ask for removal the service with API [{}]", endpoint);
-            }
-            try {
-                aLoadBalanced.exchange(
-                        endpoint,
-                        HttpMethod.DELETE,
-                        null,
-                        Void.class,
-                        Map.of("pKey", pKeys)
-                );
-                LOGGER.info("Service [{}] removed Locations", si.getServiceId());
-            } catch (Exception e) {
-                throw new RemovalNotAllowedException(format("Exception. Removal of Locations is declined by service [%s]", si.getServiceId()));
-            }
+    private void removeLocation(ReplicaRegistry srv, Collection<String> pKeys) {
+        var headers = new HttpHeaders();
+        var endpoint = "http://" + srv.getApplicationName() + srv.getRemovalEndpoint();
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Ask for removal the service with API [{}]", endpoint);
+        }
+        try {
+            aLoadBalanced.exchange(
+                    endpoint,
+                    HttpMethod.DELETE,
+                    new HttpEntity<List<String>>(new ArrayList<>(pKeys), headers),
+                    Void.class
+            );
+            LOGGER.info("Service [{}] removed Locations", srv.getApplicationName());
+        } catch (Exception e) {
+            throw new RemovalNotAllowedException(format("Exception. Removal of Locations is declined by service [%s]", srv.getApplicationName()));
         }
     }
 }
